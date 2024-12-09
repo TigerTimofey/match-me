d5d9ab70c685ccb42f396f05485ce8095bd46886
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-12-09 07:40:08 +0200

    first get req and res & clean project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
+async function start() {
+  const PORT = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
-bootstrap();
+start();
